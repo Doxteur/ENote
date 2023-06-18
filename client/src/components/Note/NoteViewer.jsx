b53340bbc903299ee932fb2c2667fe2cc1fe474b
jimmy doussain <doxteur>
2023-06-18 23:09:32 +0200

    Ajout Design

--- client/src/components/Note/NoteViewer.jsx
+++ client/src/components/Note/NoteViewer.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate,Link } from "react-router-dom";
 import { socket } from "../../utils/socket";
 
 function NoteViewer() {
   const [isConnectedIo, setIsConnectedIo] = useState(false);
   const [text, setText] = useState("");
+  const navigate = useNavigate();
+	const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
     socket.on("connect", () => {
@@ -25,9 +29,18 @@ function NoteViewer() {
       console.log(data);
     });
 
-    fetch("http://localhost:4001/api/rooms/42")
+    fetch("http://localhost:4001/api/rooms/42", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${auth.token}`
+      },
+    })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log(err);
+      });
 
     return () => {
       socket.close();
@@ -55,8 +68,8 @@ function NoteViewer() {
   };
 
   return (
-    <div>
-      <h1>Text</h1>
+    <div className="min-w-screen min-h-screen bg-gray-200">
+      {/* <h1>Text</h1>
       <input
         type="text"
         className="border border-black"
@@ -64,7 +77,72 @@ function NoteViewer() {
           handleChange(e.target.value);
         }}
         value={text}
-      />
+      /> */}
+
+      <div className="flex items-center justify-center px-5 py-5 pt-24">
+        <div
+          className="w-full max-w-6xl mx-auto rounded-xl bg-white shadow-lg p-5 text-black"
+        >
+          <div className="border border-gray-200 overflow-hidden rounded-md">
+            <div className="w-full flex border-b border-gray-200 text-xl text-gray-600">
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-bold"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-italic"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-underline"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-paragraph"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-header-1"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-header-2"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-header-3"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-list-bulleted"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-list-numbered"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-align-left"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-align-center"></i>
+              </button>
+              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                <i className="mdi mdi-format-align-right"></i>
+              </button>
+            </div>
+            <div className="w-full p-2 h-96">
+              <textarea className="w-full h-full focus:outline-none border-none"
+                onChange={(e) => {
+                  handleChange(e.target.value);
+                }}
+                value={text}
+              ></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Link className="btn btn-error btn-sm float-right mx-40" 
+      onClick={() => {
+        socket.close();
+        socket.removeAllListeners();
+        localStorage.removeItem("token");
+      }}
+      to="/"
+      >
+        Se déconnecter
+      </Link>
     </div>
   );
 }
