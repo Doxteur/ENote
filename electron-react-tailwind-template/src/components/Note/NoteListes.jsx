e977ec9fb13f8f3ec2a7f3c98fcc310bf2393093
Icare741 <enzo.lebuhotel@hotmail.fr>
2023-07-04 12:50:58 +0200

    add front slidebar and post design

--- electron-react-tailwind-template/src/components/Note/NoteListes.jsx
+++ electron-react-tailwind-template/src/components/Note/NoteListes.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getNotes } from "../../features/Notes/NotesReducer";
+import SideBar from "../SideBar/SideBar";
+
 
 function NoteListes() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-	const notes = useSelector((state) => state.notes);
+  const notes = useSelector((state) => state.notes);
   const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -19,83 +21,169 @@ function NoteListes() {
 
   return (
     <div className="bg-gray-200 h-screen">
-      <div className="grid grid-cols-4 p-20">
-        <div className="p-6">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <img className="w-full" src="/mountain.jpg" alt="Mountain" />
-            <div className="px-6 py-4 ">
-              <div className="font-bold text-xl mb-2">Mountain</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
+
+      <div className="sticky flex h-screen flex-row gap-4 overflow-y-auto rounded-lg sm:overflow-x-hidden">
+        <SideBar />
+        <div className="flex flex-row flex-wrap gap-4 p-6 ">
+          <div className="my-4 grid w-full grid-cols-3 gap-4 ">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitationserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <button className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 float-right hover:bg-green-500 hover:text-white "
-              onClick={
-                e => handleEdit("42")
-              }>
-                Editer
-              </button>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit  consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="p-6">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <img className="w-full" src="/mountain.jpg" alt="Mountain" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Mountain</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis , sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <button className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 float-right hover:bg-green-500 hover:text-white ">
-                Editer
-              </button>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipqui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="p-6">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <img className="w-full" src="/mountain.jpg" alt="Mountain" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Mountain</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud elit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <button className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 float-right hover:bg-green-500 hover:text-white ">
-                Editer
-              </button>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incidsse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="p-6">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <img className="w-full" src="/mountain.jpg" alt="Mountain" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Mountain</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, Nonea! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum lum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <button className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 float-right hover:bg-green-500 hover:text-white ">
-                Editer
-              </button>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor i ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Loremit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Lorem i cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-header">Maximizing Your Productivity at Work</h2>
+                <p className="text-content2">Loremnt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="card-footer">
+                  <button className="btn-secondary btn" onClick={
+                    e => handleEdit("42")
+                  }>Edit</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+
+
+
     </div>
   );
 }
--- electron-react-tailwind-template/src/components/Note/NoteViewer.jsx
+++ electron-react-tailwind-template/src/components/Note/NoteViewer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, Link, useLocation } from "react-router-dom";
 import { socket } from "../../utils/socket";
+import SideBar from "../SideBar/SideBar";
 
 function NoteViewer() {
   const [isConnectedIo, setIsConnectedIo] = useState(false);
@@ -80,69 +81,87 @@ function NoteViewer() {
 
   return (
     <div className="min-w-screen min-h-screen bg-gray-200">
-      {/* <h1>Text</h1>
-      <input
-        type="text"
-        className="border border-black"
-        onChange={(e) => {
-          handleChange(e.target.value);
-        }}
-        value={text}
-      /> */}
 
-      <div className="flex items-center justify-center px-5 py-5 pt-24">
-        <div className="w-full max-w-6xl mx-auto rounded-xl bg-white shadow-lg p-5 text-black">
-          <div className="border border-gray-200 overflow-hidden rounded-md">
-            <div className="w-full flex border-b border-gray-200 text-xl text-gray-600">
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-bold"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-italic"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-underline"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-paragraph"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-header-1"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-header-2"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-header-3"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-list-bulleted"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-list-numbered"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-align-left"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-align-center"></i>
-              </button>
-              <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
-                <i className="mdi mdi-format-align-right"></i>
-              </button>
+      <div className="sticky flex h-screen flex-row gap-4 overflow-y-auto rounded-lg sm:overflow-x-hidden">
+        <SideBar />
+        <div className="flex items-center justify-center px-5 py-5 pt-24">
+          <div className="w-full max-w-6xl mx-auto rounded-xl bg-white shadow-lg p-5 text-black">
+            <div className="border border-gray-200 overflow-hidden rounded-md">
+              <div className="w-full flex border-b border-gray-200 text-xl text-gray-600">
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-bold"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-italic"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-underline"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-paragraph"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-header-1"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-header-2"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-header-3"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-list-bulleted"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 mr-1 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-list-numbered"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-l border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-align-left"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-align-center"></i>
+                </button>
+                <button className="outline-none focus:outline-none border-r border-gray-200 w-10 h-10 hover:text-indigo-500 active:bg-gray-50">
+                  <i className="mdi mdi-format-align-right"></i>
+                </button>
+              </div>
+              <div className="w-full p-2 h-96">
+                <textarea
+                  className="w-full h-full focus:outline-none border-none"
+                  onChange={(e) => {
+                    handleChange(e.target.value);
+                  }}
+                  value={text}
+                ></textarea>
+              </div>
             </div>
-            <div className="w-full p-2 h-96">
-              <textarea
-                className="w-full h-full focus:outline-none border-none"
-                onChange={(e) => {
-                  handleChange(e.target.value);
-                }}
-                value={text}
-              ></textarea>
-            </div>
+            <Link
+              className="btn btn-error btn-sm float-right mx-40"
+              onClick={() => {
+                socket.close();
+                socket.removeAllListeners();
+                localStorage.removeItem("token");
+              }}
+              to="/"
+            >
+              Se déconnecter
+            </Link>
+            <Link
+              className="btn btn-success btn-sm float-left mx-40"
+              onClick={() => {
+                socket.close();
+                socket.removeAllListeners();
+                localStorage.removeItem("token");
+              }}
+              to="/notes"
+            >
+              Vos documents
+            </Link>
           </div>
         </div>
       </div>
+
+
       <Link
         className="btn btn-error btn-sm float-right mx-40"
         onClick={() => {
@@ -163,7 +182,7 @@ function NoteViewer() {
         }}
         to="/notes"
       >
-        Vos documennts
+        Vos documents
       </Link>
     </div>
   );
