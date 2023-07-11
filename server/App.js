dfdec47897f473ff89ec3a9ab224cca3f3ff3646
Jdoussain <jdoussain@linaia.com>
2023-07-11 22:27:07 +0200

    Fix Api

--- server/App.js
+++ server/App.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:4001"],
+    origin: ["http://localhost:3000", "http://localhost:4001", "http://192.168.0.37:3000","http://192.168.0.43:3000"],
   },
 });
 
